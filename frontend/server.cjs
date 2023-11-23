d4a23c93c486a131e73249fe2ce57741859db392
khushikalra08 <111592002+khushikalra08@users.noreply.github.com>
2023-11-23 18:42:48 +0530

    Add files via upload

---
+++ frontend/server.cjs
@@ -0,0 +1,17 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+// Serve static files from the 'build' directory
+app.use(express.static(path.join(__dirname, 'build')));
+
+// All routes should redirect to the index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
